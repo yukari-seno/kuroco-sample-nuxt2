769b995ec3a8c3a50b3014490443b6f5b08683f3
kobayashi <kobayashi@mexess.co.jp>
2023-07-14 15:25:36 +0900

    エラー制御追加

---
+++ pages/erro.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>エラーが発生しました。</h1>
+</template>
+<script>
+export default {
+  mounted() {
+    window.onload = () => {
+      document.location.href = process.env.ERROR_URL
+    }
+  }
+}
+</script>
