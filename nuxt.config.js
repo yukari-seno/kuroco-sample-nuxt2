c82c84ed007cc38b54fe5d33507d40bf011a573a
kobayashi <kobayashi@mexess.co.jp>
2023-07-14 19:52:43 +0900

    トークン追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -73,7 +73,8 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: process.env.BASE_URL
+    baseURL: process.env.BASE_URL,
+    headers: { 'x-rcms-api-access-token': process.env.TOKEN }
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
--- plugins/axios.js
+++ plugins/axios.js
@@ -1,10 +1,4 @@
 export default function({ $axios }) {
-  const token = process.env.TOKEN
-  $axios.onRequest((config) => {
-    config.headers = { 'x-rcms-api-access-token': token }
-    return config
-  })
-
   $axios.onError((err) => {
     // axiosの通信でエラーが発生した時にエラーページにリダイレクト
     console.log(err)
